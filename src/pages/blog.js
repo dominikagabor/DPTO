63508aee1e7a47ee1f970751e025feb5ede40b29
unknown <dominikagabor190@gmail.com>
2019-11-30 13:31:52 +0100

    eee

---
+++ src/pages/blog.js
@@ -0,0 +1,29 @@
+export default ({
+    data: {
+        allMarkdownRemark: { edges },
+    },
+}) => {
+    return (
+        <Layout>
+        </Layout>
+    );
+}
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "YYYY-MM-DD")
+            path
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+  `
