558c53fb844301cf0ce02d4e15db71d064e2f59e
unknown <dominikagabor190@gmail.com>
2019-11-30 13:52:20 +0100

    iii

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout } from '../containers/Layout';
+import { Layout } from '../containers/layout';
 import { graphql } from "gatsby";
 import PostLink from '../components/Postlink';
 
